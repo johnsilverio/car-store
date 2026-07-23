7d25c3e6067e160d07592174bbb46db03008c0fe
John Silverio <johnvitor676@gmail.com>
2026-07-23 17:04:58 -0300

    refact: migrate cars/views.py from functions based views to class based views

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 
 from accounts.views import login_view, logout_view, register_view
 from app import settings
-from cars.views import cars_view, new_car_view
+from cars.views import CarsView, NewCarView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
-    path("cars/", cars_view, name="cars_list"),
-    path("new-car/", new_car_view, name="new_car"),
+    path("cars/", CarsView.as_view(), name="cars_list"),
+    path("new-car/", NewCarView.as_view(), name="new_car"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,31 +1,32 @@
 from django.db.models import Q
-from django.http import HttpRequest
 from django.shortcuts import redirect, render
+from django.views import View
 
 from cars.forms import CarForm
 from cars.models import Car
 
 
-def cars_view(request: HttpRequest):
-    search = request.GET.get("search")
+class CarsView(View):
+    def get(self, request):
+        search = request.GET.get("search")
 
-    if search:
-        cars = Car.objects.filter(
-            Q(model__icontains=search) | Q(brand__name__icontains=search)
-        ).order_by("model")
-    else:
-        cars = Car.objects.all().order_by("model")
+        if search:
+            cars = Car.objects.filter(
+                Q(model__icontains=search) | Q(brand__name__icontains=search)
+            ).order_by("model")
+        else:
+            cars = Car.objects.all().order_by("model")
 
-    return render(request, "cars.html", {"cars": cars})
+        return render(request, "cars.html", {"cars": cars})
 
 
-def new_car_view(request: HttpRequest):
-    if request.method == "POST":
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarForm()
+        return render(request, "new-car.html", {"new_car_form": new_car_form})
+
+    def post(self, request):
         new_car_form = CarForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect("cars_list")
-    else:
-        new_car_form = CarForm()
-
-    return render(request, "new-car.html", {"new_car_form": new_car_form})
