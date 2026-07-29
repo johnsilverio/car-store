96ffaf4904748e374bb13bbd068df22852ad8d66
John Silverio <johnvitor676@gmail.com>
2026-07-29 16:08:00 -0300

    feat: signals config car_pre_save

--- cars/signals.py
+++ cars/signals.py
@@ -1,8 +1,14 @@
 from django.db.models import Sum
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 
 from cars.models import Car, CarInventory
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.description:
+        instance.description = "Descrição gerada automaticamente!"
 
 
 @receiver(post_save, sender=Car)
