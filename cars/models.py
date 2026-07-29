2f09ed9c48f76a0b219d75cea434a3e8440d0ee5
John Silverio <johnvitor676@gmail.com>
2026-07-29 15:15:24 -0300

    feat: using Django Signals

--- cars/models.py
+++ cars/models.py
@@ -18,6 +18,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     photo = models.ImageField(upload_to="cars/", blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
